2b7068d2d7cbc202896823c93b8dd32920d3744f
Dragon777 <chris.igel@outlook.de>
2021-07-18 11:43:59 +0200

    🚀 Frame updated

--- src/main/java/de/dragon/FTClient/frame/FTPFrame.java
+++ src/main/java/de/dragon/FTClient/frame/FTPFrame.java
@@ -44,8 +44,6 @@ public class FTPFrame {
     private Task task;
     private boolean isInit = false;
 
-    //Test commit
-
     public FTPFrame() throws IOException, ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
